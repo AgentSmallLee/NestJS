229894f15250c31d6f5fd103b76e3b7ad301a4e8
lixiaoniu <1615404043@qq.com>
2026-08-28 05:49:14 +0800

    chore: move notes to notes folder and add PostgreSQL connection guide
    - Move 3 existing note files to notes/ directory
    - Add PostgreSQL connection troubleshooting note
    - Include user module changes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { UserService } from './user.service.js';
 import { User } from './user.js';
+import { log } from 'console';
 
 @Controller('user')
 export class UserController {
@@ -13,6 +14,30 @@ export class UserController {
 
   @Post('create')
   createUser(@Body() user: User): User {
+    log(user);
     return this.userService.createUser(user);
+  }
+
+  @Get('user/:id')
+  getUserById(@Param('id') id: number): User {
+    return this.userService.getUserById(id);
+  }
+
+  @Get('list')
+  getUserList(@Query('name') name: string): User[] {
+    log(name);
+    return this.userService.getUserList(name);
+  }
+
+  @Put('user/:id')
+  updateUser(@Param('id') id: string,@Body() user: User) {
+    log(user);
+    return this.userService.updateUser( id,user);
+  }
+
+  @Delete('user/:id')
+  deleteUser(@Param('id') id: string) {
+    log(id);
+    return this.userService.deleteUser( id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,57 @@
 import { Injectable } from '@nestjs/common';
 import { User } from './user.js';
+import { log } from 'console';
 
 @Injectable()
 export class UserService {
-    
+    deleteUser(id: string) {
+       log(id)
+        const index = this.users.findIndex(u => u.id === parseInt(id));
+        if(index === -1){
+            return {
+                "success": false,
+                "message": `user ${id} not found`,
+            };
+        }
+        this.users.splice(index,1);
+        return {
+            "success": true,
+            "message": `user ${id} deleted`,
+        };
+    }
+
+    private users: User[] = [
+        new User(1,"zhangsan",18),
+        new User(2,"lisi",18),
+    ];
+
+    updateUser(id: string, user: User) {
+        log(this.users)
+        const index = this.users.findIndex(u => u.id === parseInt(id));
+        if(index === -1){
+            return {
+                "success": false,
+                "message": `user ${id} not found`,
+            };
+        }
+        if(index !== -1){
+            this.users[index] = user;
+        }
+        return user;
+    }
+
+    getUserList(name: string): User[] {
+        return [new User(1,name,18),new User(2,name,18)];
+    }
+
     getUser(): string {
         return 'this is user!';
     }
     createUser(user: User): User {
         return new User(1,"zhangsan",18);
+    }
+
+    getUserById(id: number): User {
+        return new User(id,"zhangsan",18);
     }
 }
